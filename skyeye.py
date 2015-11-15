416b66df9c6b52b4cc4ad7f83f3d7591fe07d5ab
cainiaocome <cainiaocome@gmail.com>
2015-11-15 21:47:13 +0800

    0.01

--- skyeye.py
+++ skyeye.py
@@ -10,15 +10,15 @@ from Queue import Queue
 import threading
 
 from utils import ip2int, int2ip
-from config import dport, sport
+from config import dport, sport, rtt
 
 total_packet_sent = 0
 time_to_send = 1
 lock = threading.Lock()
 
-to_send_queue = Queue(maxsize=300)
+to_send_queue = Queue(maxsize=300) #(ip)
 wait_ans_list = []  # (ip, port, time)
-sniffed_packet_queue = Queue(maxsize=0)
+sniffed_packet_queue = Queue(maxsize=0) # (pkt, time)
 
 def random_ip_generator():
     while True:
@@ -30,28 +30,33 @@ def send_probe():
     while True:
         ip = to_send_queue.get(block=True)
         to_send_queue.task_done()
-        p = IP(dst=ip)/TCP(dport=dport, flags='S')
-        print 'send to {}'.format(ip)
+        p = IP(dst=ip)/TCP(sport=sport, dport=dport, flags='S')
         lock.acquire()
         wait_ans_list.append((ip, dport, time.time()))
         lock.release()
         send(p,verbose=0)
+        #print 'send to {}'.format(ip)
 
 def sniff_packet():
     def _prn(x):
-        print 'sniffed_packet_queue size:', sniffed_packet_queue.qsize()
+        #print 'sniffed_packet_queue size:', sniffed_packet_queue.qsize()
         sniffed_packet_queue.put((x, time.time()), block=True)
-    sniff(filter='tcp dst port {}'.format(sport), prn=_prn, store=0)
+    def _lfilter(pkt):
+        if pkt.haslayer(TCP) and pkt[TCP].dport==sport and pkt[TCP].sport==dport:
+            return True
+        else:
+            #print 'bad packet'
+            return False
+    sniff(lfilter=_lfilter, prn=_prn, store=0)
 
 def check_ans():
     while True:
         p = sniffed_packet_queue.get(block=True)
-        p_src_ip = p.sprintf('%IP.src%')
-        p_src_port = int(p.sprintf('%IP.sport%'))
-        p_dst_ip = p.sprintf('%IP.dst%')
-        p_dst_port = int(p.sprintf('%IP.dport%'))
+        p_src_ip = p[0].sprintf('%IP.src%')
+        p_src_port = int(p[0].sprintf('%r,TCP.sport%'))
+        p_dst_ip = p[0].sprintf('%IP.dst%')
+        p_dst_port = int(p[0].sprintf('%r,TCP.dport%'))
         sniffed_packet_queue.task_done()
-        print 'check_ans got packet'
 
         lock.acquire()
         for index, x in enumerate(wait_ans_list):
@@ -59,11 +64,7 @@ def check_ans():
             if (t-x[2])>rtt:
                 del(wait_ans_list[index])
             # todo check the packet
-            x_src_ip = x.sprintf('%IP.src%')
-            x_src_port = int(p.sprintf('%IP.sport%'))
-            x_dst_ip = x.sprintf('%IP.dst%')
-            x_dst_port = int(p.sprintf('%IP.dport%'))
-            if p_src_ip==x_dst_ip and p_src_port==x_dst_port and p_dst_port==x_src_port:
+            if p_src_ip==x[0]:
                 print '{}:{} open'.format(p_src_ip, p_src_port)
                 del(wait_ans_list[index])
         lock.release()
@@ -73,11 +74,17 @@ def main():
     thread_2 = threading.Thread(target=send_probe)
     thread_3 = threading.Thread(target=sniff_packet)
     thread_4 = threading.Thread(target=check_ans)
-    thread_1.daemon = thread_2.daemon = thread_3.daemon = thread_4.daemon = True
+    thread_1.setDaemon(True)
+    thread_2.setDaemon(True)
+    thread_3.setDaemon(True)
+    thread_4.setDaemon(True)
     thread_1.start()
     thread_2.start()
     thread_3.start()
     thread_4.start()
+    while True:
+        time.sleep(10)
+        pass
 
 if __name__=='__main__':
     main()
